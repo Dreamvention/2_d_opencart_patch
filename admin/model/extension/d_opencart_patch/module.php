a2866dfce078211df5019c025cee365746290e53
dmitriyzhuk <dmitriyzhuk@gmail.com>
2018-09-17 09:45:49 +0300

    added return $this->db->getLastId(); to admin -> addModule

--- admin/model/extension/d_opencart_patch/module.php
+++ admin/model/extension/d_opencart_patch/module.php
@@ -6,7 +6,7 @@ class ModelExtensionDOpencartPatchModule extends Model {
         }else{
             $this->db->query("INSERT INTO `" . DB_PREFIX . "module` SET `name` = '" . $this->db->escape($data['name']) . "', `code` = '" . $this->db->escape($code) . "', `setting` = '" . $this->db->escape(serialize($data)) . "'");
         }
-        
+        return $this->db->getLastId();
     }
     
     public function editModule($module_id, $data) {
