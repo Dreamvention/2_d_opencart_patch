d9f5b970be3245f6dcb457a2aee30e29ffeb78c0
pavel <php5.drvn@gmail.com>
2018-12-27 10:16:42 +0200

    fix settingValue func

--- admin/model/extension/d_opencart_patch/setting.php
+++ admin/model/extension/d_opencart_patch/setting.php
@@ -19,10 +19,20 @@ class ModelExtensionDOpencartPatchSetting extends Model {
         $this->load->model('setting/setting');
         return $this->model_setting_setting->deleteSetting($code, $store_id);
     }
-    public function getSettingValue($key, $store_id = 0)
-    {
-        $this->load->model('setting/setting');
-        return $this->model_setting_setting->getSettingValue( $key, $store_id);
+    public function getSettingValue($key, $store_id = 0){
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '" . (int)$store_id . "' AND `key` = '" . $this->db->escape($key) . "'");
+        if (empty($query->num_rows)) {
+            return null;
+        }
+        if (!$query->row['serialized']) {
+            return $query->row['value'];
+        } else {
+            if (VERSION >= '2.2.0.0') {
+                return json_decode($query->row['value'], true);
+            } else {
+                return unserialize($query->row['value']);
+            }
+        }
     }
     public function editSettingValue($code = '', $key = '', $value = '', $store_id = 0) {
         $this->load->model('setting/setting');
